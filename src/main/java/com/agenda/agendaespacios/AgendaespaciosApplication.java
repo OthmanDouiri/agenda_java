60ea2ddb84f26f61c3f89504702bde9c1f7afcc5
Othman Git <odouiri@elpuig.xeill.net>
2025-05-13 17:48:28 +0200

    first commit

---
+++ src/main/java/com/agenda/agendaespacios/AgendaespaciosApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.agendaespacios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaespaciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaespaciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenda/agendaespacios/AgendaespaciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.agendaespacios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaespaciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
